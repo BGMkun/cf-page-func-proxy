2a503edea710aa28c7904cee3fb7456bf53875db
BGMkun <x2683396087@live.cn>
2022-05-21 11:33:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="a.futa.gq";
+        url.hostname="a.futanari.cf";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
